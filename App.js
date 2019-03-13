d5fe47930cc685ee007f55d1a54b96c59ab6a11b
Kevin Tran <ktran@Kevins-MacBook-Pro.local>
2019-03-13 17:41:30 -0400

    [Navigation] Delete comments in app.js. Replaced tab navigation with left sidebar

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+
 import Login from './screens/LoginScreen';
+
 
 export default class App extends React.Component {
   state = {
@@ -33,16 +35,6 @@ export default class App extends React.Component {
           onLoginPress={() => this.setState({isLoggedIn: true})}
           />);
       }
-
-
-/*
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-      */
     }
   }
 
