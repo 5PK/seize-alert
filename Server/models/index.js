cd286f6d46d72f4d8a875fdb3017017027a9e8e4
Kevin Tran <ktran@bashx3.ca>
2019-03-31 23:11:04 -0400

    [ServerSetup] Add ORM, and began rudimentary server setup

---
+++ Server/models/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'mysql',
+  },
+);
+
+const models = {
+  User: sequelize.import('./user'),
+  Contact: sequelize.import('./contact'),
+};
+
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export { sequelize };
+
+export default models;
