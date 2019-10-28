1d22ff92d105ddf9eb1123d93ef72cf49ee53d1e
5PK <me@ktran.info>
2019-10-28 12:40:13 -0400

    merged heartbeat code into project

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -24,11 +24,48 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import SensorsComponent from './SensorsComponent';
+import { connect } from 'react-redux';
+import Heartbeat from './Heartbeat';
+import heart from './heart.png';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  view: {
+    flex: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'gray',
+    padding: 10,
+    margin: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
 
 const App = () => {
   return (
     <View>
       <SensorsComponent></SensorsComponent>
+      <View style={styles.view}>
+        <Image source={heart} style={{ width: imageSize, height: imageSize }} resizeMode="contain" />
+      </View>
+      <View style={styles.view}>
+        <TouchableOpacity style={styles.button} onPress={() => Heartbeat.startService()}>
+          <Text style={styles.instructions}>Start</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => Heartbeat.stopService()}>
+          <Text style={styles.instructions}>Stop</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- ReactNativeProject/index.js
+++ ReactNativeProject/index.js
@@ -5,5 +5,24 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import { setHeartBeat, store } from './store';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+const MyHeadlessTask = async () => {
+    console.log('Receiving HeartBeat!');
+    store.dispatch(setHeartBeat(true));
+    setTimeout(() => {
+      store.dispatch(setHeartBeat(false));
+    }, 1000);
+  };
+
+const RNRedux = () => (
+    <Provider store={store}>
+      <App />
+    </Provider>
+);
+
+
+AppRegistry.registerHeadlessTask('Heartbeat', () => MyHeadlessTask);
+AppRegistry.registerComponent(appName, () => RNRedux);
