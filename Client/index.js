32c57ba61901eb76df835db336251da5df1d8781
rhancox4303 <riley.hancox@mohawkcollege.ca>
2019-11-11 20:24:02 -0500

    Detected by id

---
+++ Client/index.js
@@ -0,0 +1,25 @@
+import { AppRegistry } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
+import App from './App';
+import { name as appName } from './app.json';
+import { setHeartBeat, store } from './store';
+import Bluetooth from './Bluetooth';
+const MyHeadlessTask = async () => {
+    console.log('Start');
+    var bl = new Bluetooth()
+    bl.requestPermission()
+    bl.scanAndConnect()
+
+
+};
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+
+AppRegistry.registerHeadlessTask('Heartbeat', () => MyHeadlessTask);
+AppRegistry.registerComponent(appName, () => RNRedux);
