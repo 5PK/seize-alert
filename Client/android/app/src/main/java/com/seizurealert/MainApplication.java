fec3f9ccd544f59f9aa33ae121879ba36474d09e
rhancox4303 <riley.hancox@mohawkcollege.ca>
2019-12-02 17:25:32 -0500

    Implement Filesystem

--- Client/android/app/src/main/java/com/seizurealert/MainApplication.java
+++ Client/android/app/src/main/java/com/seizurealert/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.polidea.reactnativeble.BlePackage;
+import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,6 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNFSPackage(),
           new BlePackage(),
           new SeizurePackage()
       );
--- Client/index.js
+++ Client/index.js
@@ -6,51 +6,72 @@ import { name as appName } from './app.json';
 import { setSeizureDetection, store } from './store';
 import Bluetooth from './Bluetooth';
 import { Device } from 'react-native-ble-plx';
-
+import RNFS from 'react-native-fs';
 console.log('Start');
 
+var path = RNFS.ExternalDirectoryPath + '/data.txt';
+
+RNFS.writeFile(path, 'Data Collection SeizeAlert', 'utf8')
+  .then((success) => {
+    console.log('FILE WRITTEN!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+console.log(path);
 const MyHeadlessTask = async () => {
 
-    stopProcessing = false
+  stopProcessing = false
 
-    var bl = new Bluetooth(["98:07:2D:26:6D:02","54:6C:0E:52:CF:DC"])
+  // var bl = new Bluetooth(["98:07:2D:26:6D:02","54:6C:0E:52:CF:DC"])
 
-    await bl.requestPermission()
-    var devices = await bl.startDeviceScan()
-    
-    bl.connectDevices()
+  // await bl.requestPermission()
+  // var devices = await bl.startDeviceScan()
 
-    zeroData = []
-    oneData = []
+  // bl.connectDevices()
 
-    while(!stopProcessing){
-      if(devices[0].isDeviceConnected && devices[1].isDeviceConnected ){
-        if(bl.oneIsProcessing && bl.zeroIsProcessing){
-          setTimeout(function(){
-            var datetime = new Date();
-            var timestamp = datetime.toISOString();
-            zeroData = bl.zeroData
-            oneData = bl.oneData
-            
-            fetch(`http://localhost:3000/api/data`, {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    ZeroId: "98:07:2D:26:6D:02",
-                    ZeroData: zeroData,
-                    OneId: "54:6C:0E:52:CF:DC",
-                    OneData: oneData,
-                    Timestamp: timestamp
-                })
-            });
-          },1000)
-        }
-      }
-    }
-      
+  // zeroData = []
+  // oneData = []
+
+  while (!stopProcessing) {
+    console.log("Test")
+    //if(devices[0].isDeviceConnected && devices[1].isDeviceConnected ){
+    //if(bl.oneIsProcessing && bl.zeroIsProcessing){
+    setTimeout(function () {
+
+      RNFS.write(path, 'Test', 'utf8')
+        .then((success) => {
+          console.log('FILE WRITTEN!');
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+      // var datetime = new Date();
+      // var timestamp = datetime.toISOString();
+      // zeroData = bl.zeroData
+      // oneData = bl.oneData
+
+      // fetch(`http://localhost:3000/api/data`, {
+      //     method: "POST",
+      //     headers: {
+      //         Accept: "application/json",
+      //         "Content-Type": "application/json"
+      //     },
+      //     body: JSON.stringify({
+      //         ZeroId: "98:07:2D:26:6D:02",
+      //         ZeroData: zeroData,
+      //         OneId: "54:6C:0E:52:CF:DC",
+      //         OneData: oneData,
+      //         Timestamp: timestamp
+      //     })
+      // });
+    }, 1000)
+  }
+  //}
+  //}
+
 };
 
 const RNRedux = () => (
